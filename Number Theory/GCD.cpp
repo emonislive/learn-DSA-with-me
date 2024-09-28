63d86e64b8b761e9fdcdb5213ab7f033da09bedf
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-09-28 14:26:39 +0600

    GCD (Greatest Common Divisor) or HCF (Highest Common Factor)

--- Number Theory/GCD.cpp
+++ Number Theory/GCD.cpp
@@ -1,8 +1,8 @@
 // GCD (Greatest Common Divisor) or HCF (Highest Common Factor)
 #include <iostream>
 using namespace std;
-
 #define endl '\n'
+
 void gcd() {
     int a, b, mod;
     cout << "Enter Two Numbers : ";
@@ -15,6 +15,7 @@ void gcd() {
     }
     printf("Greatest Common Divisor GCD(%d,%d): %d\n", tmp1, tmp2, a);
 }
+
 int main() {
     gcd();
     return 0;
