35384a77722312c47f48f3f93576f84767e0fef0
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-10-06 13:24:18 +0600

    Count Divisors of a Number

--- Number Theory/Divisor.cpp
+++ Number Theory/Divisor.cpp
@@ -3,14 +3,29 @@
 using namespace std;
 #define endl '\n'
 
+/*
+    for ex. n = 10, sqrt(10) = 3. loop will me with in 1..to..3, & divisors of 10 = 1, 2, 5, 10
+    so after reducting the loop length, we will get this ans 1, 2 from the loop but 5, 10 is missing
+    in that case, if (i != n / i) condition comes in. so , if i is divisible then (n/i) is also divisible
+    ex. n = 10, i = 1, (10 / 1) = 10, i = 2, (10 / 2) = 5, now we can add the missing 5, 10.
+    != is used because for ex. The divisors of 36 are: 1, 2, 3, 4, 6, 9, 12, 18, 36.
+    now when i = 6, if (n % i == 0) this condition will be true, so c will increase by 1
+    then again it will check if (i != n / i) this condition (36/6) = 6, so again the condition will be true
+    so to counter the duplicates the != is added.
+*/
+
+
 void solve(int n)
 {
     int c = 0;
-    // this is the efficient way to find the total divisor of a number
     for (int i = 1; i <= sqrt(n); i++) 
     {
-        if (n % i == 0)
+        if (n % i == 0) 
+        {
             c++;
+            if (i != n / i)
+                c++;
+        }
     }
     /* this is the brute force method
     for (int i = 1; i <= n; i++)
