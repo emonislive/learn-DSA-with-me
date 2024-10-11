b73e6db81b2882f896103870cec54cdc2e0e99f1
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-10-11 12:50:48 +0600

    Linear Search.cpp

--- Data Structures/Linear Search.cpp
+++ Data Structures/Linear Search.cpp
@@ -9,29 +9,22 @@ void linearSearch(int arr[], int len, int search) {
             cout << search << " found at position " << i + 1 << endl;
             return;
         }
-        /*
-        int c = 0;
-        if (arr[i] == search) {
-             // * this condition if there are duplicates in an array.
-             c++;
-             cout << search << " found at position " << i + 1 ", ";
-        }
-        cout << endl;
-        if (c == 0)
-            cout << "Not Found" << endl;
-        */
     }
     cout << "Not Found" << endl;
 }
 int main() {
     int len, search;
+    
     cout << "Enter The Length of the Array: ";
     cin >> len;
+    
     int arr[len];
+    
     cout << "Enter The Array: ";
     for (int i = 0; i < len; i++) {
         cin >> arr[i];
     }
+    
     cout << "Enter a value to search: ";
     cin >> search;
     linearSearch(arr, len, search);
